6b650a271f960e742c8765ac946dc708c4b2da97
s6411423101 <s6411423101@pkru.ac.th>
2024-06-14 17:32:06 +0700

    n

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,6 @@ export default function RootLayout({
         )}
       >
         {/* sidebar */}
-        {/* <p className="border">Sidebar</p> */}
         <div className="border">
           <SideNavbar />
         </div>
