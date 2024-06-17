c8401f0fa46f9247c8340cdfb5ebf557b72832e3
s6411423101 <s6411423101@pkru.ac.th>
2024-06-17 15:57:28 +0700

    PKRU LOGO

--- components/SideNavbar.tsx
+++ components/SideNavbar.tsx
@@ -2,15 +2,9 @@
 
 import { useState } from "react";
 import { Nav } from "./ui/nav";
-
+import Image from "next/image";
 type Props = {};
-import {
-  SlidersVertical,
-  ChevronRight,
-  Newspaper,
-  CircleGauge,
-  FileUp,
-} from "lucide-react";
+import { SlidersVertical, Newspaper, CircleGauge, FileUp } from "lucide-react";
 import { Button } from "./ui/button";
 
 import { useWindowWidth } from "@react-hook/window-size";
@@ -26,36 +20,39 @@ export default function SideNavbar({}: Props) {
   }
 
   return (
-    <div className="relative min-w-[150px] border-r px-7  pb-20 pt-40">
-      <Nav
-        links={[
-          {
-            title: "Dashbord",
-            icon: CircleGauge,
-            href: "/dashbord",
-            variant: "default",
-          },
-          {
-            title: "Report",
-            icon: Newspaper,
-            href: "/report",
-            variant: "ghost",
-          },
-          {
-            title: "Export",
-            icon: FileUp,
-            href: "/export",
-            variant: "ghost",
-          },
-          {
-            title: "Setting",
-            icon: SlidersVertical,
-            href: "/setting",
-            variant: "ghost",
-          },
-        ]}
-        isCollapsed={false}
-      />
+    <div className="relative min-w-[150px] border-r px-9  pb-11 pt-16">
+      <Image src="/pkru.png" width={170} height={200} alt="PKRU LOGO" />
+      <div className="pt-20">
+        <Nav
+          links={[
+            {
+              title: "Dashbord",
+              icon: CircleGauge,
+              href: "/dashbord",
+              variant: "default",
+            },
+            {
+              title: "Report",
+              icon: Newspaper,
+              href: "/report",
+              variant: "ghost",
+            },
+            {
+              title: "Export",
+              icon: FileUp,
+              href: "/export",
+              variant: "ghost",
+            },
+            {
+              title: "Setting",
+              icon: SlidersVertical,
+              href: "/setting",
+              variant: "ghost",
+            },
+          ]}
+          isCollapsed={false}
+        />
+      </div>
     </div>
   );
 }
